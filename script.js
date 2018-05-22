1ed9c46b8384fc4ecf0903ff3b3a1ca498b45b95
CreativeShin <creative-shin@outlook.com>
2018-05-22 22:55:01 +0530

    hamburger

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 document.querySelector('.menu').addEventListener('click', function(){
     document.querySelector('.navigation').classList.toggle('clicked');
+    document.querySelector('.menu').classList.toggle('cross');
 });
