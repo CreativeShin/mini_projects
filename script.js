a6db77627dcaab90ed0a4acacd3169bf69233477
CreativeShin <creative-shin@outlook.com>
2018-05-23 09:34:46 +0530

    nav scroll

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-document.querySelector('.menu').addEventListener('click', function(){
-    document.querySelector('.navigation').classList.toggle('clicked');
-    document.querySelector('.menu').classList.toggle('cross');
+const navigation = document.querySelector('.navigation');
+const navEl = document.querySelector('nav');
+const menu = document.querySelector('.menu');
+document.querySelector('.menu').addEventListener('click', ()=>{
+    navigation.classList.toggle('clicked');
+    menu.classList.toggle('cross');
+});
+
+window.addEventListener('scroll', ()=>{
+    if(window.scrollY){
+        navEl.style.background = "#444";
+        navEl.style.height = "60px"; 
+    }else{
+        navEl.style.background = "none";
+    }
 });
